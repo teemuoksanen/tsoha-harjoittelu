e5dfa3e6adfa8361199b46f95c5b110d60b127a5
Teemu Oksanen <github@teemuoksanen.fi>
2020-05-17 17:38:53 +0300

    Osa 3.3 tehty

--- application/tasks/views.py
+++ application/tasks/views.py
@@ -1,16 +1,21 @@
-from application import app, db
 from flask import redirect, render_template, request, url_for
+from flask_login import login_required
+
+from application import app, db
 from application.tasks.models import Task
+from application.tasks.forms import TaskForm
 
 @app.route("/tasks", methods=["GET"])
 def tasks_index():
     return render_template("tasks/list.html", tasks = Task.query.all())
 
 @app.route("/tasks/new/")
+@login_required
 def tasks_form():
-    return render_template("tasks/new.html")
+    return render_template("tasks/new.html", form = TaskForm())
 
 @app.route("/tasks/<task_id>/", methods=["POST"])
+@login_required
 def tasks_set_done(task_id):
 
     t = Task.query.get(task_id)
@@ -20,8 +25,16 @@ def tasks_set_done(task_id):
     return redirect(url_for("tasks_index"))
 
 @app.route("/tasks/", methods=["POST"])
+@login_required
 def tasks_create():
-    t = Task(request.form.get("name"))
+    form = TaskForm(request.form)
+
+    if not form.validate():
+        return render_template("tasks/new.html", form = form)
+
+    t = Task(form.name.data)
+    t.done = form.done.data
+    t.account_id = current_user.id
 
     db.session().add(t)
     db.session().commit()
