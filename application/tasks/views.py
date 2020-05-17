e4f3e79b5cac92c24f9442eaa37d0c4f53d5a591
Teemu Oksanen <github@teemuoksanen.fi>
2020-05-17 18:14:40 +0300

    PostgreSQL käyttöön

--- application/tasks/views.py
+++ application/tasks/views.py
@@ -1,5 +1,5 @@
 from flask import redirect, render_template, request, url_for
-from flask_login import login_required
+from flask_login import login_required, current_user
 
 from application import app, db
 from application.tasks.models import Task
