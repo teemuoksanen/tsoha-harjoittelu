3c82661e1a5167dd88d4aa280f53cd8a11215ec1
Teemu Oksanen <github@teemuoksanen.fi>
2020-05-17 18:18:10 +0300

    PostgreSQL käyttöön, jatko

--- application/__init__.py
+++ application/__init__.py
@@ -4,14 +4,14 @@ app = Flask(__name__)
 
 # Tuodaan SQLAlchemy käyttöön
 from flask_sqlalchemy import SQLAlchemy
-# Käytetään tasks.db-nimistä SQLite-tietokantaa. Kolme vinoviivaa
-# kertoo, tiedosto sijaitsee tämän sovelluksen tiedostojen kanssa
-# samassa paikassa
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///tasks.db"
-# Pyydetään SQLAlchemyä tulostamaan kaikki SQL-kyselyt
-app.config["SQLALCHEMY_ECHO"] = True
 
-# Luodaan db-olio, jota käytetään tietokannan käsittelyyn
+import os 
+if os.environ.get("HEROKU"):
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///tasks.db"
+    app.config["SQLALCHEMY_ECHO"] = True
+
 db = SQLAlchemy(app)
 
 # Luetaan kansiosta application tiedoston views sisältö
@@ -40,4 +40,7 @@ def load_user(user_id):
     return User.query.get(user_id)
 
 # Luodaan lopulta tarvittavat tietokantataulut
-db.create_all()
+try:
+    db.create_all()
+except:
+    pass
